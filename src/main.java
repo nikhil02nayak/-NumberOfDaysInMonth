27e5729878aa726e2b647eae1620ce8e314092cb
Nikhil S N <73348331+nikhil02nayak@users.noreply.github.com>
2020-10-23 19:51:41 +0530

    Add files via upload
    Problem statement:
    
    Write a method isLeapYear with a parameter of type int named year. 
    
    The parameter needs to be greater than or equal to 1 and less than or equal to 9999.
    
    If the parameter is not in that range return false. 
    
    Otherwise, if it is in the valid range, calculate if the year is a leap year and return true if it is, otherwise return false. 
    
    Write another method getDaysInMonth with two parameters month and year.  ​Both of type int.
    
    If parameter month is < 1 or > 12 return -1. ​
    If parameter year is < 1 or > 9999 then return -1.
    
    This method needs to return the number of days in the month. Be careful about leap years they have 29 days in month 2 (February).
    
    You should check if the year is a leap year using the method isLeapYear described above.

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println(LeapYear.isLeapYear(2018) );
+        System.out.println(LeapYear.getDaysInMonth(2,-2018));
+
+
+
+    }
+}
